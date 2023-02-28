7245c51e115030bcbc73075beecfffa6da29bdd7
fel <felicien.dehertogh@protonmail.com>
2023-03-01 00:38:52 +0100

    create nav

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,34 @@
 <script setup lang="ts"></script>
 
 <template>
-  <header>
-    <div></div>
-  </header>
+  <nav>
+    <RouterLink to="/">Coins</RouterLink>
+    <RouterLink to="/exchanges">Exchanges</RouterLink>
+  </nav>
+  <RouterView />
 </template>
 
-<style scoped></style>
+<style scoped>
+nav {
+  width: 100%;
+  font-size: 20px;
+  text-align: center;
+  padding: 1rem;
+  background-color: black;
+}
+
+/* nav a.router-link-exact-active {
+  color: var(--color-text);
+} */
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  text-decoration: none;
+  color: var(--color-text);
+}
+</style>
