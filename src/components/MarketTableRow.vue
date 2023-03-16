e4cc327e1238c693d45d936993dac65d2b82388a
fel <felicien.dehertogh@protonmail.com>
2023-03-16 15:16:19 +0100

    Create MarketTableRow component

--- src/components/MarketTableRow.vue
+++ src/components/MarketTableRow.vue
@@ -1,36 +1,18 @@
 <template>
-  <div class="table">
-    <table cellspacing="0" cellpadding="0">
-      <col width="100" />
-      <col width="100" />
-      <col width="100" />
-      <thead>
-        <tr>
-          <th class="left-aligned" colspan="2">Name</th>
-          <th class="right-aligned">Price</th>
-          <th class="right-aligned">Change</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td class="left-aligned" colspan="2">
-            <div>
-              <div>{{ id }}</div>
-              <p>{{ symbol }}</p>
-            </div>
-          </td>
-          <td class="right-aligned">${{ priceUsd }}</td>
-          <td class="right-aligned">{{ changePercent24Hr }}%</td>
-        </tr>
-        <MarketTableRow />
-      </tbody>
-    </table>
-  </div>
+  <tr>
+    <td class="left-aligned" colspan="2">
+      <div>
+        <div>{{ id }}</div>
+        <p>{{ symbol }}</p>
+      </div>
+    </td>
+    <td class="right-aligned">${{ priceUsd }}</td>
+    <td class="right-aligned">{{ changePercent24Hr }}%</td>
+  </tr>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
-import MarketTableRow from './MarketTableRow.vue'
 
 // const assets = ref<
 //   Array<{ id: string; symbol: string; changePercent24Hr: string; priceUsd: string }>
@@ -66,31 +48,9 @@ onMounted(() => getData())
 </script>
 
 <style scoped>
-table {
-  width: 100%;
-  background-color: var(--color-background);
-  margin-top: 1rem;
-  border-radius: 8px;
-  /* border-collapse: collapse; */
-}
-
-thead {
-  color: var(--vt-c-text-dark-2);
-  background-color: black;
-  border-radius: 8px;
-}
-
-th {
-  padding: 1rem;
-}
-
 td {
   background-color: var(--color-background);
   padding: 0.5rem 1rem;
-}
-
-tbody > tr {
-  outline: 1px solid;
 }
 
 .left-aligned {
