999593e25add4fe048d09448595fdf977f4be299
fel <felicien.dehertogh@protonmail.com>
2023-03-18 20:59:31 +0100

    Add token icons

--- src/components/MarketTableRow.vue
+++ src/components/MarketTableRow.vue
@@ -3,9 +3,12 @@
     <td class="left-aligned">{{ rank }}</td>
 
     <td class="left-aligned" colspan="2">
-      <div>
-        <p class="name">{{ name }}</p>
-        <p class="symbol">{{ symbol }}</p>
+      <div class="">
+        <img :src="iconUrl" alt="" />
+        <div class="coin">
+          <p class="name">{{ name }}</p>
+          <p class="symbol">{{ symbol }}</p>
+        </div>
       </div>
     </td>
     <td class="right-aligned">${{ priceUsd }}</td>
@@ -16,13 +19,15 @@
 </template>
 
 <script setup lang="ts">
-import { ref, watchEffect } from 'vue'
+import { ref, watchEffect, computed } from 'vue'
 import type { Asset } from '../types/interfaces'
 
 const symbol = ref('')
+const lowerCaseSymbol = ref('')
 const priceUsd = ref('')
 const changePercent24Hr = ref('')
 const rank = ref('')
+const id = ref('')
 const name = ref('')
 const supply = ref('')
 const maxSupply = ref('')
@@ -42,13 +47,20 @@ const props = defineProps<{
 
 watchEffect(() => {
   rank.value = props.asset.rank
+  id.value = props.asset.id
   name.value = props.asset.name
   symbol.value = props.asset.symbol
+  lowerCaseSymbol.value = symbol.value.toLowerCase()
   priceUsd.value = trimString(props.asset.priceUsd)
   marketCapUsd.value = trimString(props.asset.marketCapUsd)
   changePercent24Hr.value = trimString(props.asset.changePercent24Hr)
   volumeUsd24Hr.value = trimString(props.asset.volumeUsd24Hr)
 })
+
+// const iconUrl = computed(() => `https://cryptoicons.org/api/color/${lowerCaseSymbol.value}/200`)
+const iconUrl = computed(
+  () => `https://assets.coincap.io/assets/icons/${lowerCaseSymbol.value}@2x.png`
+)
 </script>
 
 <style scoped>
@@ -72,5 +84,16 @@ td {
 
 .name {
   font-weight: 500;
+}
+
+.coin {
+  display: inline-block;
+}
+
+img {
+  display: inline-block;
+  height: 30px;
+  width: 30px;
+  margin-right: 0.5em;
 }
 </style>
